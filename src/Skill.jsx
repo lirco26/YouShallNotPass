e620ee13d6d6f110f952ab3b3da2f0e300d88602
Lir <lircohen26@gmail.com>
2020-03-24 18:30:12 +0200

    refactor logic

--- src/Skill.jsx
+++ src/Skill.jsx
@@ -17,8 +17,6 @@ export default class Skill extends React.Component {
         this.subPoint = this.subPoint.bind(this);
         this.onInputBlur = this.onInputBlur.bind(this);
         this.onInputChange = this.onInputChange.bind(this);
-
-        this.state = {validationClass: ''};
     }
 
     addPoint() {
@@ -32,11 +30,6 @@ export default class Skill extends React.Component {
     onInputChange(event) {
         const newValue = Number(event.target.value);
         this.props.setPointsValue(newValue);
-        if (newValue !== this.getFixedSkillPoints(newValue)) {
-            this.setState({validationClass: 'invalid-input'});
-        } else {
-            this.setState({validationClass: ''});
-        }
     }
 
     onInputBlur(evt) {
@@ -50,23 +43,21 @@ export default class Skill extends React.Component {
     }
 
     getFixedSkillPoints(value) {
-        this.setState({validationClass: ''});
-        if (value < MIN_POSSIBLE_POINTS) {
-            return MIN_POSSIBLE_POINTS;
-        }
-        if (value > MAX_POSSIBLE_POINTS) {
-            return MAX_POSSIBLE_POINTS;
-        }
-        return value;
+        return Math.min(MAX_POSSIBLE_POINTS, Math.max(MIN_POSSIBLE_POINTS, value));
     }
 
     render() {
+        let inputClassName = '';
+        if (this.getFixedSkillPoints(this.props.pointsValue) !== this.props.pointsValue) {
+            inputClassName = 'invalid-input';
+        }
+
         return <div className="skill">
             {this.props.name}
             <div className="skill-points">
                 <button onClick={this.addPoint}>+</button>
                 <input
-                    className={this.state.validationClass}
+                    className={inputClassName}
                     type="number"
                     value={this.props.pointsValue}
                     onChange={this.onInputChange}
