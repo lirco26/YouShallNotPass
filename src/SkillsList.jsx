9b530ade2d4b62d7cf0c827934ab012b1f2b1f93
Lir <lircohen26@gmail.com>
2020-03-24 17:30:00 +0200

    refactor logic

--- src/SkillsList.jsx
+++ src/SkillsList.jsx
@@ -1,32 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Skill from './Skill.jsx'
+import Skill from './Skill.jsx';
 
-export default class SkillsList extends React.Component {
-    static propTypes = {
-        updateSkillPoints: PropTypes.func.isRequired,
-        skillsNamesAndValues: PropTypes.object.isRequired,
-    };
+const _ = require('lodash');
 
-    getListOfSkills() {
-        const skillsList = [];
-        for (let skillName of Object.keys(this.props.skillsNamesAndValues)) {
-            const skill = <Skill
-                key={skillName}
-                name={skillName}
-                value={this.props.skillsNamesAndValues[skillName]}
-                updatePointsStatus={(points) => {
-                    this.props.updateSkillPoints(skillName, points)
-                }}
-            />;
-            skillsList.push(skill);
-        }
-        return skillsList;
-    }
+export default function SkillsList(props) {
+    const skillsList = _.map(props.skillsNamesToPoints, (skillPoints, skillName) => {
+        return <Skill
+            name={skillName}
+            value={skillPoints}
+            updatePointsStatus={(points) => props.updateSkillPoints(skillName, points)}
+        />;
+    });
 
-    render() {
-        return <div className="skills-list">
-            {this.getListOfSkills()}
-        </div>;
-    }
+    return <div className="skills-list">
+        {skillsList}
+    </div>;
+
 }
+
+SkillsList.propTypes = {
+    updateSkillPoints: PropTypes.func.isRequired,
+    skillsNamesToPoints: PropTypes.object.isRequired,
+};
