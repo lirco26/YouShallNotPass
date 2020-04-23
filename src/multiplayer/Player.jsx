2df6b29ba856281cac885a008f6ddb4e96fecd3e
Lir <lircohen26@gmail.com>
2020-04-24 02:07:52 +0300

    open body game when pressing a players button

--- src/multiplayer/Player.jsx
+++ src/multiplayer/Player.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import CLASSES from "./classes";
 import GameBody from '../single_user_game/GameBody.jsx';
@@ -7,17 +7,21 @@ import TriggeredPresenter from "./TriggeredPresenter";
 
 // maybe Player should save the state of its GameBody so it could be related
 export default function Player({name, imageSrc, playerClass}) {
+    const [isGameBodyOpen, setIsGameBodyOpen] = useState(false);
+
     function openGameBodyForUser() {
+        setIsGameBodyOpen(true);
     }
 
     return <div>
-        <button className="user" onClick={openGameBodyForUser}>
+        <button className={isGameBodyOpen? "user-card pressed" : "user-card"} onClick={openGameBodyForUser}>
             <img src={imageSrc}/>
             <div className="user-details">
                 <h2>{name}</h2>
                 {playerClass}
             </div>
         </button>
+        {isGameBodyOpen && <GameBody />}
     </div>;
 }
 
