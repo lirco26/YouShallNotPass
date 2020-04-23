ecac4f1f0d5a3c0d520ba529f682cc2412a71890
Lir <lircohen26@gmail.com>
2020-04-23 21:01:13 +0300

    use CLASSES instead of free text. Also dont allow open another card or add a card whose name already exists

--- src/multiplayer/PlayerList.jsx
+++ src/multiplayer/PlayerList.jsx
@@ -1,53 +1,63 @@
 import React, {useState} from 'react';
 
 import Player from './Player.jsx';
+import CLASSES from "./classes";
 
 
 export default function usePlayerList() {
     const [playerList, setPlayerList] = useState([]);
 
     function addPlayer(name, imageSrc, playerClass) {
-        const newPlayer = <Player
-            key={name}
-            imageSrc={imageSrc}
-            name={name}
-            playerClass={playerClass}
-        />;
-        setPlayerList(prevPlayerList => {
-            const newPlayerList = [...prevPlayerList];
-            newPlayerList.push(newPlayer);
-            return newPlayerList;
-        })
+        if (playerList.map(player => player.key).includes(name)) {
+            alert('This name already exists. Please choose another name');
+        }
+        else {
+            const newPlayer = <Player
+                key={name}
+                imageSrc={imageSrc}
+                name={name}
+                playerClass={playerClass}
+            />;
+            console.log((newPlayer.key));
+            setPlayerList(prevPlayerList => {
+                const newPlayerList = [...prevPlayerList];
+                newPlayerList.push(newPlayer);
+                return newPlayerList;
+            })
+        }
     }
 
     return [playerList, addPlayer];
 }
 
 export function AddPlayerButton({addPlayer}) {
-    return <button className="add-player-button" onClick={addPlayer}> + Add player </button>;
+    return <button className="add-player-button" onClick={addPlayer}>
+        + Add player
+    </button>;
 }
 
 export function AddPlayerForm({addPlayer}) {
     const [name, setName] = useState('');
-    const [playerClass, setClass] = useState('');
+    const [playerClass, setClass] = useState(Object.keys(CLASSES)[0]);
 
     return <div className="add-player">
-        name:
+        Name:
         <input
             className="add-player-input"
             type="text" value={name}
             onChange={event => setName(event.target.value)}
         />
-        class:
-        <input
+        Class:
+        <select
             className="add-player-input"
-            type="text"
-            value={playerClass}
-            onChange={event => setClass(event.target.value)}
-        />
+            onChange={event => setClass(event.target.value)}>
+            {Object.keys(CLASSES).map(key =>
+                <option value={key}> {key} </option>
+            )}
+        </select>
         <button className="submit-button" onClick={() => {
             if (name && playerClass)
-                addPlayer(name, "", playerClass);
+                addPlayer(name.toUpperCase(), `userImages/${playerClass}.jpeg`, playerClass);
         }}> Submit
         </button>
     </div>
--- src/multiplayer/TriggeredPresenter.jsx
+++ src/multiplayer/TriggeredPresenter.jsx
@@ -1,29 +1,42 @@
 import React, {useState} from "react";
 
 import usePlayerList, {AddPlayerButton, AddPlayerForm} from './PlayerList.jsx';
+import GameBody from '../single_user_game/GameBody';
+
 
 
 /**
  * this component presents a triggered component:
  * A. An add-player-form
- * B. A player's game-body
+ * B. A player's game-body - [received as props to the component???]
  */
-export default function TriggeredPresenter() {
+export default function TriggeredPresenter({gameBody}) {
+    const [isAddingPlayer, setIsAddingPlayer] = useState(false);
     const [isPresenting, setIsPresenting] = useState(false);
     const [playerList, addPlayer] = usePlayerList();
 
-    function triggeredShowComponent() {
-        setIsPresenting(true);
+    function triggeredAddPlayer() {
+        if(!isPresenting) {
+            setIsPresenting(true);
+            setIsAddingPlayer(true);
+        }
+        else {
+            alert('You cannot open another card. Submit your changes first and then start something else');
+        }
     }
 
     function doneShowComponent() {
         setIsPresenting(false);
+        setIsAddingPlayer(false);
+    }
+
+    function triggredSetPlayerGameBody() {
     }
 
     return <div>
         {playerList}
-        <AddPlayerButton addPlayer={triggeredShowComponent}/>
-        {isPresenting && <AddPlayerForm addPlayer={(name, imageSrc, playerClass) => {
+        <AddPlayerButton addPlayer={triggeredAddPlayer}/>
+        {isAddingPlayer && <AddPlayerForm addPlayer={(name, imageSrc, playerClass) => {
             addPlayer(name, imageSrc, playerClass);
             doneShowComponent();
         }}/>}
